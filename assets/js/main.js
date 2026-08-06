338c12bee95e99311e006233ca98792ea4edace1
IanHP <129088911+IanHP@users.noreply.github.com>
2026-08-05 21:26:35 -0400

    Feature: images will all be same height (maximized) Feature: added in 3 column insert. Used in main html

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function equalizeHeights() {
+  /*Creates a class (photo-row) that can be applied to a column
+  space div (a section that holds multiple columns).
+  
+  It will edit every image so that: all images will have the same height
+  as the "shortest" image. 
+  */
+  const rows = document.querySelectorAll('.photo-row');
+
+  rows.forEach(row => {
+    const imgs = row.querySelectorAll('img');   // only this row's images
+    imgs.forEach(img => img.style.height = 'auto');   // reset
+
+    let min = Infinity;
+    imgs.forEach(img => min = Math.min(min, img.clientHeight));  // min for THIS row
+
+    imgs.forEach(img => {
+      img.style.height = min + 'px';
+      //img.style.objectFit = 'cover'; // crops the images
+      img.style.width = 'auto'; // keeps aspect ratio with new set height
+    });
+  });
+}
+
+window.addEventListener('load', equalizeHeights);
+window.addEventListener('resize', equalizeHeights);
